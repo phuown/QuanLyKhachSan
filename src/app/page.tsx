90a3d3ca7ccd235aac5e0eb1d62032e5390ec5bd
phuown <pthuphuong051103@gmail.com>
2026-03-24 15:47:04 +0700

    Chia them footer cho web

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,13 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-// import SearchRoom from "@/components/SearchRoom";
-// import RoomList from "@/components/RoomList";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main>
-
       <Header />
-
       <Hero />
-
-      {/* <SearchRoom />
-
-      <RoomList /> */}
-
+      <Footer />
     </main>
   );
 }
