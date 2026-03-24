0acc03669116b2863b462f03bc7da411e4789f79
phuown <pthuphuong051103@gmail.com>
2026-03-24 17:15:44 +0700

    Layout chinh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,7 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -27,7 +22,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className="font-sans antialiased"
         >
           {children}
         </body>
